2ec3c903176ea7c6a82882cb2f57abd3e0460f99
Mckz33 <mackenzie.max.rj@gmail.com>
2023-12-29 12:07:36 -0300

    first commit

---
+++ src/main/java/com/ementa_equivalencia/ia/javaementaequivalenciaia/JavaEmentaEquivalenciaIaApplication.java
@@ -0,0 +1,13 @@
+package com.ementa_equivalencia.ia.javaementaequivalenciaia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaEmentaEquivalenciaIaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaEmentaEquivalenciaIaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ementa_equivalencia/ia/javaementaequivalenciaia/JavaEmentaEquivalenciaIaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ementa_equivalencia.ia.javaementaequivalenciaia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaEmentaEquivalenciaIaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
